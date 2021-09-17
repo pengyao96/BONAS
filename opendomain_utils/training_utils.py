5fafbcdad343ef271aec3f7aca4a0c7724225cef
yaopeng <Kuaishou20210520@>
2021-09-17 13:23:51 +0800

    xx

--- opendomain_utils/training_utils.py
+++ opendomain_utils/training_utils.py
@@ -120,7 +120,8 @@ def load(model, model_path):
 def drop_path(x, drop_prob):
   if drop_prob > 0.:
     keep_prob = 1.-drop_prob
-    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+    feat = torch.cuda.FloatTensor(x.size(0), 1, 1, 1) if torch.cuda.is_available() else torch.FloatTensor(x.size(0), 1, 1, 1)
+    mask = Variable(feat).bernoulli_(keep_prob)
     x.div_(keep_prob)
     x.mul_(mask)
   return x
