45480ec279fd0ea3a7a5cfde7559b5576516b8ea
yaopeng <Kuaishou20210520@>
2021-09-21 18:23:42 +0800

    xx

--- fully_train/parallel_train_cifar10.py
+++ fully_train/parallel_train_cifar10.py
@@ -7,15 +7,20 @@ import os
 import subprocess
 import random
 python_path = 'python'  # path of python command
-gpu_num = 8
+gpu_num = 6
 tasks = []
 
 for gpu in range(gpu_num):
     seed = random.randint(1,10000)
-    cmd = [python_path, os.path.join('./', 'train_cifar10.py'), f"--gpu={gpu}", f"--arch=exp_BONAS_10",
-           f"--save={gpu+26}", "--auxiliary", f"--seed={seed}"]
+    cmd = [python_path, os.path.join('./', 'train_cifar10.py'), f"--gpu={gpu}", f"--arch=exp_BONAS_13",
+           f"--save={gpu+34}", "--auxiliary", f"--seed={seed}"]
     train = subprocess.Popen(cmd)
     tasks.append(train)
 for task in tasks:
     task.wait()
 print(f"Num of Tasks finished:{len(tasks)}")
+
+
+
+
+#python train_cifar10.py --gpu=0 --arch=exp_BONAS_13 --save=34 --auxiliary --seed=123
